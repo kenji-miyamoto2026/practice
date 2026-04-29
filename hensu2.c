bde835f5876fd7ccc2c7848f340da6c618c3486a
kenji-miyamoto2026 <omksk0428@gmail.com>
2026-04-29 22:07:12 +0900

    #3 文字型変数　１６進数変換

---
+++ hensu2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+  char size;
+  int value;
+  
+  size = 'M';
+   = 100;
+  
+  printf("size =  %c\n",size);
+  printf("１６進数表示で%xです。\n",value);
+  
+  return 0;
+}
